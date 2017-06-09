abc149163405910ca1e19b34544ed7e3bb1e5c85
toyofuku-syunya <syunya0921@akane.waseda.jp>
2017-06-09 19:31:39 +0900

    Initial Commit

---
+++ YugiohMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YugiohMap
+//
+//  Created by Syunya Toyofuku on 2017/06/09.
+//  Copyright © 2017年 Syunya Toyofuku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
